d1d1035f986e24a02877a1289da4c78ab1b81eac
mistastone <michael.stone2@gmail.com>
2015-01-31 18:05:34 -0500

    First Update

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+$title = "Front Page";
+$page_name = "frontpage"; 
+include('includes/header.php');
+
+?>
+
+<body>
+
+<div class="line"></div>
+
+<section class="container-fluid meetmario">
+
+	<div class="meettext">
+			<div class="col-lg-4 col-lg-offset-3">
+				<h2>Meet Mario Racco</h2>
+				<p>I know the riding very well, and have spent more than 30 years working with people throughout the community. I want to continue this work on your behalf in Ottawa, helping to plan and deliver initiatives that are important to you as residents and will make Canada a better place to live, work and play.</p>
+			</div>
+		</div>
+
+	<div class="meetimage">
+		<div class="col-lg-2">
+		<img src="images/mario.jpg" class="img-responsive" alt="Mario G. Racco">
+		</div>
+	</div>
+
+</section>
+
+<div class="line"></div>
+
+</body>
